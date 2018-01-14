ab62914fcf276741bc652bf406e9d6a5a63f926f
hudsonjl <hudsonjl@iu.edu>
2018-01-14 15:42:21 -0500

    uploading database file

---
+++ app/darksky_database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE active_sensors
+	(
+	serial varchar(200),
+	latitude decimal(10, 8),
+	longitude decima(11,8),
+	location varchar(500),
+	primary key(serial));
+
+CREATE TABLE archived_sensors
+	(
+	serial varchar(200),
+	latitude decimal(10, 8),
+	longitude decima(11,8),
+	location varchar(500),
+	primary key(serial));
+
+CREATE TABLE users
+	(
+	username varchar(25)
+	password varchar(200));
