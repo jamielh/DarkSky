1ad29fc8f87bd985715e2cd0a40a1feffba064d8
Ben Stueve <bstueve@indiana.edu>
2018-04-09 20:14:09 -0400

    working on map information

--- app/index2.php
+++ app/index2.php
@@ -64,24 +64,24 @@
                  content: contentPoint" . $sensor['sensor_id'] . "
                });
 
-               var marker" . $sensor['sensor_id'] . " = new google.maps.Marker({
-                 position: p" . $sensor['sensor_id'] . ",
-                 map: map
-               });
 
-               marker" . $sensor['sensor_id'] . ".addListener('click', function() {
-                 infoPoint" . $sensor['sensor_id'] . ".open(map, marker" . $sensor['sensor_id'] . ");
-               });
 
-			   var myCity = new google.maps.Circle({
-  		   		center:p" . $sensor['sensor_id'] . ",
-  				radius:20000,
+			   var circle" . $sensor['sensor_id'] . " = new google.maps.Circle({
+
   				strokeColor:'#FF0000',
   				strokeOpacity:0.8,
   				strokeWeight:2,
   				fillColor:'#FF0000',
-  				fillOpacity:0.4
-}); ";
+  				fillOpacity:0.4,
+				map: map,
+				center:p" . $sensor['sensor_id'] . ",
+				radius:5000,
+				clickable: true
+			});
+
+			circle" . $sensor['sensor_id'] . ".addListener('click', function() {
+			  infoPoint" . $sensor['sensor_id'] . ".open(map, circle" . $sensor['sensor_id'] . ");
+			});";
         	}
         	mysqli_close($con);
           ?>
