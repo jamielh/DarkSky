04bbfbc7598dc173d4c30ce0aa89a24e6a4de77e
hudsonjl <hudsonjl@iu.edu>
2018-04-11 11:32:18 -0400

    added download all data functionality

--- app/download.php
+++ app/download.php
@@ -1,35 +1,40 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="index.css" rel="stylesheet" type="text/css">
-<title>Download Data</title>
-</head>
-
-<body>
 <?php
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'a');
+
+$headers = array("Timestamp", "Sensor ID", "Hour 1 Reading", "Hour 2 Reading", "Hour 3 Reading", "Hour 4 Reading" , "Hour 5 Reading" ,
+			"Hour 6 Reading", "Hour 7 Reading", "Hour 8 Reading", "Hour 9 Reading", "Hour 10 Reading", "Hour 11 Reading", "Hour 12 Reading",
+			"Hour 13 Reading", "Hour 14 Reading", "Hour 15 Reading","Hour 16 Reading", "Hour 17 Reading", "Hour 18 Reading", "Hour 19 Reading",
+			"Hour 20 Reading", "Hour 21 Reading", "Hour 22 Reading", "Hour 23 Reading", "Hour 24 Reading") ;
+// output the column headings
+fputcsv($output, $headers);
+
 //make database connection
 $con = mysqli_connect("db.soic.indiana.edu", "i494f17_team45", "my+sql=i494f17_team45", "i494f17_team45");
 if (!$con){die("Failed to connect to MySQL: " . mysqli_connect_error()); }
-$result = "SELECT * FROM sensors;";
-$sensors = mysqli_query($con, $result);
-while ($sensor = mysqli_fetch_assoc($sensors)) {
-	$times= "SELECT MAX(time_stamp), readings FROM sensor_data WHERE sensor_id = 'darksky_" . $sensor['sensor_id'] . "';";
-	$sensorInfo = mysqli_query($con, $times);
-	$data = mysqli_fetch_assoc($sensorInfo);
-	$data = $data['readings'];
-	$data = str_replace("[", "", $data);
-	$data = str_replace("]", "", $data);
-	$data = explode(", ", $data);
-	echo $data
-<!--navigation menu goes at the top of every page on the site-->
-<div class="menu">
-<ul class="menu">
-	<li><a href="index.php">Home</a></li>
-	<li><a href="graphsAll.php">Graphs and Charts</a></li>
-	<li><a class="active" href="weather.html">Weather</a></li>
-	<li><a href="about.html">About</a></li>
-</ul>
-</div>
-
-
-
-</body>
-</html>
+$query = "SELECT * FROM sensor_data;";
+$result = mysqli_query($con, $query);
+if ($result->num_rows > 0) {
+//arrays for characting replacing later
+$badchars = ["]", "["];
+$goodchars = ["", ""];
+	
+while($row = $result->fetch_assoc()) {
+//this can print the data with all 24 hours in one column - fputcsv($output, $row);
+//build array to send to csv
+$new_array = array($row["time_stamp"], $row["sensor_id"]);
+//get rid of brackets
+$clean_readings = str_replace($badchars, $goodchars, $row['readings']);
+//create an array of all the sensor readings
+$readings_list = explode(", ", $clean_readings) ;
+//iterate over array of readings and add each to the end of the array that we send to csv
+foreach ($readings_list as $reading) {
+array_push ($new_array, $reading);}
+//copy array to csv
+fputcsv($output, $new_array);	
+}}
+?>
